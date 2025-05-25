2eae94b35d620d67c5735c235260f8632b58b7e8
Dale Hutchinson <daletristanhutchinson@gmail.com>
2025-05-26 09:37:27 +1000

    Create python wrapper for Rust function transform_image

--- backend/rust/src/python/python.rs
+++ backend/rust/src/python/python.rs
@@ -1,5 +1,5 @@
-use crate::{crop_image, resize_image, resize_image_bytes};
-// #[cfg(feature = "python")]
+use crate::{crop_image, resize_image, resize_image_bytes, transform_image};
+
 use pyo3::{
     exceptions::PyRuntimeError,
     prelude::*,
@@ -8,7 +8,10 @@ use pyo3::{
     wrap_pyfunction, Python,
 };
 
-// #[cfg(feature = "python")]
+use image::{DynamicImage, ImageFormat};
+
+use std::io::Cursor;
+
 #[pyfunction(name = "resize_image_bytes")]
 pub fn resize_image_bytes_py(py: Python<'_>, data: &[u8], max_dim: u32) -> PyResult<PyObject> {
     let out =
@@ -16,14 +19,12 @@ pub fn resize_image_bytes_py(py: Python<'_>, data: &[u8], max_dim: u32) -> PyRes
     Ok(PyBytes::new(py, &out).into())
 }
 
-// #[cfg(feature = "python")]
 #[pyfunction(name = "resize_image")]
 pub fn resize_image_py(_py: Python, inp: &str, outp: &str, max_dim: u32) -> PyResult<()> {
     resize_image(inp, outp, max_dim)
         .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))
 }
 
-// #[cfg(feature = "python")]
 #[pyfunction(name = "crop_image")]
 pub fn crop_image_py(
     py: Python<'_>,
@@ -37,11 +38,23 @@ pub fn crop_image_py(
     Ok(PyBytes::new(py, &out).into())
 }
 
-// #[cfg(feature = "python")]
+#[pyfunction(name = "transform_image")]
+pub fn transform_image_py(py: Python<'_>, path: &str, scale: f32) -> PyResult<PyObject> {
+    let img: DynamicImage =
+        transform_image(path, scale).map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+
+    let mut buffer = Cursor::new(Vec::new());
+    img.write_to(&mut buffer, ImageFormat::Png)
+        .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+
+    Ok(PyBytes::new(py, buffer.get_ref()).into_py(py))
+}
+
 #[pymodule(name = "zendolib")]
 pub fn zendolib_py(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(resize_image_py, m)?)?;
     m.add_function(wrap_pyfunction!(resize_image_bytes_py, m)?)?;
     m.add_function(wrap_pyfunction!(crop_image_py, m)?)?;
+    m.add_function(wrap_pyfunction!(transform_image_py, m)?)?;
     Ok(())
 }
