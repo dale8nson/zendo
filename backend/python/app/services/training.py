a60174d817b15006d399e85c7086662daaba9e06
dale8nson <daletristanhutchinson@gmail.com>
2025-08-06 09:28:18 +1000

    Update training.py

--- backend/python/app/services/training.py
+++ backend/python/app/services/training.py
@@ -1009,7 +1009,7 @@ async def train(
             if torch.cuda.is_available():
                 with accelerator.accumulate([text_encoder_1, text_encoder_2]):
                     # Convert images to latent space
-                    latents = vae.encode(batch["pixel_values"].to(dtype=weight_dtype)).latent_dist.sample().detach()
+                    latents = vae.encode(batch["pixel_values"].to(dtype=weight_dtype)).latent_dist.sample()
 
                     print(f"Line: {inspect.currentframe().f_lineno} Allocated: {torch.cuda.memory_allocated() / 1024**2:.2f} MiB")
                     print(f"Line: {inspect.currentframe().f_lineno} Reserved: {torch.cuda.memory_reserved() / 1024**2:.2f} MiB")
