a2176914ea5b0dd0461c88a2c0337e7fb98c0860
dale8nson <daletristanhutchinson@gmail.com>
2025-08-06 08:22:48 +1000

    update training.py

--- backend/python/app/services/training.py
+++ backend/python/app/services/training.py
@@ -654,11 +654,16 @@ async def train(
 
     vae = AutoencoderKL.from_pretrained(model_path, subfolder="vae", torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32).to(device)
 
+    vae.eval()
     vae.use_slicing = True
     vae.use_tiling = True
 
 
     unet = UNet2DConditionModel.from_pretrained(model_path, subfolder="unet", torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32, use_safetensors=True).to(device)
+
+    unet.eval()
+    unet.enable_gradient_checkpointing()
+
 
     initializer_token = re.search("(?<=<).+(?=>)", token)[0]
     placeholder_tokens = [token]
