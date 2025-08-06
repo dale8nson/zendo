2ff8a141f29edb7c5a5d05fd2ff82e6e00f3d256
dale8nson <daletristanhutchinson@gmail.com>
2025-08-06 12:09:30 +1000

    Update training.py

--- backend/python/app/services/training.py
+++ backend/python/app/services/training.py
@@ -654,16 +654,18 @@ async def train(
 
     tokenizer_1=CLIPTokenizer.from_pretrained(os.path.join(os.getcwd(), "../models/sdxl-base-1.0"), subfolder="tokenizer")
 
-    tokenizer_1.config.torch_dtype = torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
+    print(f"dir(tokenizer_1): {dir(tokenizer_1)")
+    print(f"dir(tokenizer_1.super()): {dir(tokenizer_1.super())")
+    # tokenizer_1.config.torch_dtype = torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
 
-    print(f"tokenizer_1.config.torch_dtype: {tokenizer_1.config.torch_dtype}")
+    # print(f"tokenizer_1.config.torch_dtype: {tokenizer_1.config.torch_dtype}")
 
     print(f"Line: {inspect.currentframe().f_lineno} Allocated: {torch.cuda.memory_allocated() / 1024**2:.2f} MiB")
     print(f"Line: {inspect.currentframe().f_lineno} Reserved: {torch.cuda.memory_reserved() / 1024**2:.2f} MiB")
 
     tokenizer_2=CLIPTokenizer.from_pretrained(model_path, subfolder="tokenizer_2")
 
-    tokenizer_2.config.torch_dtype = torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
+    # tokenizer_2.config.torch_dtype = torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
 
     print(f"Line: {inspect.currentframe().f_lineno} Allocated: {torch.cuda.memory_allocated() / 1024**2:.2f} MiB")
     print(f"Line: {inspect.currentframe().f_lineno} Reserved: {torch.cuda.memory_reserved() / 1024**2:.2f} MiB")
